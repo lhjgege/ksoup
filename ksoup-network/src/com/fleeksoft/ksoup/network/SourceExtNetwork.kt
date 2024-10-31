21a75c864963576c7c6cfbe6f98d06d44f4f633e
Sabeeh Ul Hussnain <mr.hussnain.it@gmail.com>
2024-11-01 01:31:20 +0500

    fixes issue #96

--- ksoup-network/src/com/fleeksoft/ksoup/network/SourceExtNetwork.kt
+++ ksoup-network/src/com/fleeksoft/ksoup/network/SourceExtNetwork.kt
@@ -7,4 +7,4 @@ import com.fleeksoft.ksoup.io.from
 import io.ktor.client.statement.*
 import io.ktor.utils.io.*
 
-suspend fun HttpResponse.asSourceReader() = SourceReader.from(this.bodyAsChannel().readBuffer)
+suspend fun HttpResponse.asSourceReader() = SourceReader.from(this.bodyAsChannel().readBuffer())
