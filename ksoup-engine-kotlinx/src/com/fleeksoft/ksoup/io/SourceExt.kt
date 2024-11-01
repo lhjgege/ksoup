a9fa840c47145bed66d31f798c934c85ffff3ede
Sabeeh Ul Hussnain <mr.hussnain.it@gmail.com>
2024-11-02 00:34:42 +0500

    clean up code

--- ksoup-engine-kotlinx/src/com/fleeksoft/ksoup/io/SourceExt.kt
+++ ksoup-engine-kotlinx/src/com/fleeksoft/ksoup/io/SourceExt.kt
@@ -1,8 +1,5 @@
-@file:OptIn(InternalAPI::class)
-
 package com.fleeksoft.ksoup.io
 
-import io.ktor.utils.io.*
 import kotlinx.io.Source
 import kotlinx.io.files.Path
 
@@ -13,10 +10,9 @@ import kotlinx.io.files.Path
 )
 fun SourceReader.Companion.from(source: Source): SourceReader = SourceReaderImpl(source)
 @Deprecated(
-    message = "SourceReader.Companion.from(ByteReadChannel) is deprecated, use ByteReadChannel.readBuffer.asInputStream() instead.",
+    message = "SourceReader.Companion.from(ByteReadChannel) is deprecated, use ByteReadChannel.readBuffer().asInputStream() instead.",
     level = DeprecationLevel.WARNING
 )
-fun SourceReader.Companion.from(bodyChannel: ByteReadChannel): SourceReader = SourceReaderImpl(bodyChannel.readBuffer)
 
 
 fun FileSource.Companion.from(file: Path): FileSource  = FileSourceImpl(file)
