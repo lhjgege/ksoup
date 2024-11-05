04021b28b7214d79bb4df0f3f0d31b02bee0a660
Sabeeh Ul Hussnain <mr.hussnain.it@gmail.com>
2024-11-05 20:18:27 +0500

    enable amper and disable gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,15 +3,15 @@ pluginManagement {
         mavenCentral()
         google()
         gradlePluginPortal()
-//        maven("https://packages.jetbrains.team/maven/p/amper/amper")
-//        maven("https://www.jetbrains.com/intellij-repository/releases")
-//        maven("https://packages.jetbrains.team/maven/p/ij/intellij-dependencies")
+        maven("https://packages.jetbrains.team/maven/p/amper/amper")
+        maven("https://www.jetbrains.com/intellij-repository/releases")
+        maven("https://packages.jetbrains.team/maven/p/ij/intellij-dependencies")
     }
 }
 
-/*plugins {
-    id("org.jetbrains.amper.settings.plugin").version("0.5.0-dev-992")
-}*/
+plugins {
+    id("org.jetbrains.amper.settings.plugin").version("0.5.0")
+}
 
 dependencyResolutionManagement {
     repositories {
@@ -31,8 +31,8 @@ include("ksoup-network-ktor2")
 include("ksoup-okio")
 include("ksoup-korlibs")
 include("ksoup-network-korlibs")
-include("ksoup-test")
-include("ksoup-benchmark")
+//include("ksoup-test")
+//include("ksoup-benchmark")
 
 //include("sample:shared", "sample:desktop")
 //include("sample:android", "sample:ios")
